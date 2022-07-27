ee823b442edc2d9e0ed220eb1615352e1b464357
Mahbubur Rahman Turzo <turzo@newgen-bd.com>
2022-07-27 14:19:05 +0600

    skill section added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { About, Footer, Header, More, Skills, Work } from './container/index';
-import { Navbar } from './components/index';
-import './App.scss';
+import { About, Footer, Header, More, Skills, Work } from "./container/index";
+import { Navbar } from "./components/index";
+import "./App.scss";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Header />
+      <Skills />
       <Work />
       <About />
       <More />
-      <Skills />
-      <Footer />
 
+      <Footer />
     </div>
   );
 }
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,183 @@
 module.exports = {
   email: "turzoxpress@gmail.com",
   phone: "+880 1834261758",
+
+  backendSkills: [
+    {
+      name: "NodeJS - Express",
+      strokeColor: "#D3D3D3",
+      strokeWidth: 4,
+      percent: 95,
+    },
+    {
+      name: "Python - Django",
+      strokeColor: "#D3D3D3",
+      strokeWidth: 4,
+      percent: 90,
+    },
+    {
+      name: "Python - Flask",
+      strokeColor: "#D3D3D3",
+      strokeWidth: 4,
+      percent: 95,
+    },
+  ],
+  frontendSkills: [
+    {
+      name: "React",
+      strokeColor: "#D3D3D3",
+      strokeWidth: 4,
+      percent: 90,
+    },
+    {
+      name: "React Native",
+      strokeColor: "#D3D3D3",
+      strokeWidth: 4,
+      percent: 90,
+    },
+    {
+      name: "Android - Java(Android Studio)",
+      strokeColor: "#D3D3D3",
+      strokeWidth: 4,
+      percent: 95,
+    },
+
+    {
+      name: "iOS - Swift & SwiftUI(XCode)",
+      strokeColor: "#D3D3D3",
+      strokeWidth: 4,
+      percent: 95,
+    },
+    {
+      name: "Game Development - Unity3D",
+      strokeColor: "#D3D3D3",
+      strokeWidth: 4,
+      percent: 95,
+    },
+  ],
+  languageSkills: [
+    {
+      name: "Python",
+      imgPath:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+      strokeColor: "#D3D3D3",
+      strokeWidth: 4,
+      percent: 90,
+    },
+    {
+      name: "Javascript",
+      imgPath:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+      strokeColor: "#D3D3D3",
+      strokeWidth: 4,
+      percent: 90,
+    },
+    {
+      name: "C#",
+      imgPath:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg",
+      strokeColor: "#D3D3D3",
+      strokeWidth: 4,
+      percent: 90,
+    },
+    {
+      name: "Swift",
+      imgPath:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/swift/swift-original.svg",
+      strokeColor: "#D3D3D3",
+      strokeWidth: 4,
+      percent: 90,
+    },
+    {
+      name: "C",
+      imgPath:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg",
+      strokeColor: "#D3D3D3",
+      strokeWidth: 4,
+      percent: 90,
+    },
+    {
+      name: "C++",
+      imgPath:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg",
+      strokeColor: "#D3D3D3",
+      strokeWidth: 4,
+      percent: 90,
+    },
+    {
+      name: "PHP",
+      imgPath:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg",
+      strokeColor: "#D3D3D3",
+      strokeWidth: 4,
+      percent: 90,
+    },
+  ],
+
+  devOpsSkills: [
+    {
+      name: "Ubuntu Server",
+      imgPath:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ubuntu/ubuntu-plain.svg",
+      strokeColor: "#D3D3D3",
+      strokeWidth: 4,
+      percent: 90,
+    },
+    {
+      name: "Amazon Cloud",
+      imgPath:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/amazonwebservices/amazonwebservices-original.svg",
+      strokeColor: "#D3D3D3",
+      strokeWidth: 4,
+      percent: 90,
+    },
+    {
+      name: "Google Cloud",
+      imgPath:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/googlecloud/googlecloud-original.svg",
+      strokeColor: "#D3D3D3",
+      strokeWidth: 4,
+      percent: 90,
+    },
+    {
+      name: "Docker",
+      imgPath:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg",
+      strokeColor: "#D3D3D3",
+      strokeWidth: 4,
+      percent: 90,
+    },
+    {
+      name: "Apache",
+      imgPath:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/apache/apache-original.svg",
+      strokeColor: "#D3D3D3",
+      strokeWidth: 4,
+      percent: 90,
+    },
+    {
+      name: "Load Balancing",
+      imgPath:
+        "https://www.freeiconspng.com/thumbs/load-balancer-icon/load-balancer-icon-1.png",
+      strokeColor: "#D3D3D3",
+      strokeWidth: 4,
+      percent: 90,
+    },
+    {
+      name: "Apache Proxy",
+      imgPath:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/apache/apache-original-wordmark.svg",
+      strokeColor: "#D3D3D3",
+      strokeWidth: 4,
+      percent: 90,
+    },
+    {
+      name: "HA Proxy",
+      imgPath:
+        "https://api.nuget.org/v3-flatcontainer/haproxy.streamprocessingoffload.agent/1.0.5/icon",
+      strokeColor: "#D3D3D3",
+      strokeWidth: 4,
+      percent: 90,
+    },
+  ],
 };
