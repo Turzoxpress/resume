02e44ac4a7d3eb873de1ef458843e667bb81ee30
Mahbubur Rahman Turzo <turzo@newgen-bd.com>
2022-07-27 11:13:35 +0600

    header modified

---
+++ src/constants/constants.js
@@ -0,0 +1,4 @@
+module.exports = {
+  email: "turzoxpress@gmail.com",
+  phone: "+880 1834261758",
+};
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -8,6 +8,9 @@ import { BsFillPersonLinesFill } from "react-icons/bs";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 import { AppWrap, MotionWrap } from "../../wrapper";
 import Resume from "../../assets/resume.pdf";
+import Swal from "sweetalert2";
+
+import constants from "../../constants/constants";
 
 const scaleVariants = {
   whileInView: {
@@ -21,12 +24,25 @@ const scaleVariants = {
 };
 
 const Header = () => {
+  const showEmail = () => {
+    Swal.fire({
+      icon: "success",
+      title: constants.email,
+    });
+  };
+
+  const showPhone = () => {
+    Swal.fire({
+      icon: "success",
+      title: constants.phone,
+    });
+  };
   return (
     <div id="home" className="w-full h-screen text-center">
       <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
         <div>
           <p className="uppercase text-sm tracking-widest text-gray-600 app-p">
-            LET&#39;S BUILD SOMETHING TOGETHER
+            Always thirsty to learn!
           </p>
           <h1 className="py-4 text-gray-700 app-head">
             Hi, I&#39;m{" "}
@@ -34,12 +50,12 @@ const Header = () => {
           </h1>
           <h1 className="py-2 text-gray-700 app-head">Software Engineer</h1>
           <p className="py-4 text-gray-600 sm:max-w-[70%] m-auto app-p">
-            I’m focused on building responsive fullstack web applications while
-            learning & exploring other technologies.
+            I’m focused on building awesome applications while learning &
+            exploring other technologies.
           </p>
           <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
             <a
-              href="https://www.linkedin.com/in/hansraj-saini-634864190/"
+              href="https://www.linkedin.com/in/mahbubur-rahman-turzo-483773ab"
               target="_blank"
               rel="noreferrer"
             >
@@ -48,7 +64,7 @@ const Header = () => {
               </div>
             </a>
             <a
-              href="https://github.com/Hansraj8149?tab=repositories"
+              href="https://github.com/Turzoxpress?tab=repositories"
               target="_blank"
               rel="noreferrer"
             >
@@ -56,12 +72,12 @@ const Header = () => {
                 <FaGithub />
               </div>
             </a>
-            <a href="/#contact">
+            <a onClick={showEmail}>
               <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
                 <AiOutlineMail />
               </div>
             </a>
-            <a href={Resume}>
+            <a onClick={showPhone}>
               <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
                 <BsFillPersonLinesFill />
               </div>
