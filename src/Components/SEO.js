333c77df6f02140530b56d209c8870a03453e77a
Mahbubur Rahman Turzo <turzo@newgen-bd.com>
2023-10-24 12:14:50 +0600

    meta data updated

--- src/Components/SEO.js
+++ src/Components/SEO.js
@@ -1,11 +1,60 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 
-export const SEO = ({ name, ocupation, description }) => {
+export const SEO = () => {
   return (
     <Helmet>
-      <title>{name} - {ocupation}</title>
-      <meta name="description" content={description} />
+      {/* Title */}
+      <title>Mahbubur Rahman Turzo - Full Stack Developer</title>
+
+      {/* Meta Tags */}
+      <meta
+        name="description"
+        content="Mahbubur Rahman Turzo - Full Stack Developer"
+      />
+      <meta
+        name="keywords"
+        content="Full Stack Developer, React, Node.js, Python, Android, iOS, Game Development, DevOps"
+      />
+      <meta name="author" content="Mahbubur Rahman Turzo" />
+
+      {/* OpenGraph tags */}
+      <meta
+        property="og:title"
+        content="Mahbubur Rahman Turzo - Full Stack Developer"
+      />
+      <meta
+        property="og:description"
+        content="Experienced Full Stack Developer skilled in React, Node.js, Python, and more."
+      />
+      <meta
+        property="og:image"
+        content="https://avatars.githubusercontent.com/u/5907099?v=4"
+      />
+      <meta
+        property="og:url"
+        content="https://turzoxpress.github.io/portfolio"
+      />
+
+      {/* Twitter Card tags */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta
+        name="twitter:title"
+        content="Mahbubur Rahman Turzo - Full Stack Developer"
+      />
+      <meta
+        name="twitter:description"
+        content="Experienced Full Stack Developer skilled in React, Node.js, Python, and more."
+      />
+      <meta
+        name="twitter:image"
+        content="https://avatars.githubusercontent.com/u/5907099?v=4"
+      />
+
+      {/* Link to the canonical URL */}
+      <link rel="canonical" href="https://turzoxpress.github.io/portfolio" />
+
+      {/* Additional meta tags or other SEO optimizations can be added here */}
     </Helmet>
   );
 };
